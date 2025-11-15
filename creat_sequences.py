de8081b5eabea0aa1e45ffd6f757997492836859
Bazgh <seyedeh.bazghandi@students.unibe.ch>
2025-11-15 13:24:54 +0100

    update create sequence to create prediction secuences with 1,5,... time steps. (recursive, 30 min, 60 min, ...)

--- creat_sequences.py
+++ creat_sequences.py
@@ -206,3 +206,59 @@ Y_norm = (Y - mean) / std
 save_path = "data/Ohio2018_processed/train_dataset_30.pt"
 torch.save({"X": torch.tensor(X_norm), "Y": torch.tensor(Y_norm)}, save_path)
 print("Saved normalized train_30 dataset:", save_path)
+
+
+# dataset for predicting 1 time step ahead
+# dataset for predicting 30 minutes ahead
+train_dataset1_1 = TimeSeriesWindowDataset(
+    root_dir="data/Ohio2018_processed",        # path to your main folder
+    split="train",          # or "val" or "test"
+    input_len=36,           # past 100 minutes (20 * 5min)
+    pred_len=6,             # predict next 30 minutes
+    stride=1,               # slide by 25 minutes
+    max_missing_length=12,  # skip if >60 minutes missing
+    normalize=False,         # per-file normalization
+    ewma_span=8             # smoothing parameter
+)
+train_dataset2_1 = TimeSeriesWindowDataset(
+    root_dir="data/Ohio2018_processed",        # path to your main folder
+    split="test",          # or "val" or "test"
+    input_len=36,           # past 100 minutes (20 * 5min)
+    pred_len=6,             # predict next 30 minutes
+    stride=1,               # slide by 25 minutes
+    max_missing_length=12,  # skip if >60 minutes missing
+    normalize=False,         # per-file normalization
+    ewma_span=8             # smoothing parameter
+)
+train_dataset_1 = ConcatDataset([train_dataset1_1, train_dataset2_1])
+
+# ---- STEP 2: compute global stats from *raw* training data ----
+# Concatenate tensors from underlying datasets
+X = np.concatenate(
+    [train_dataset1_1.X.numpy(), train_dataset2_1.X.numpy()],
+    axis=0
+)
+Y = np.concatenate(
+    [train_dataset1_1.Y.numpy(), train_dataset2_1.Y.numpy()],
+    axis=0
+)
+
+# STEP 2 — Compute global stats from raw training data
+mean = X.mean()
+std = X.std()
+
+print("Training mean:", mean)
+print("Training std:", std)
+
+# STEP 3 — Save global stats
+np.savez("global_train_stats.npz", mean=mean, std=std)
+
+
+#Apply global normalization (same as training)
+X_norm = (X - mean) / std
+Y_norm = (Y - mean) / std
+# STEP 5 — Save normalized dataset
+save_path = "data/Ohio2018_processed/train_dataset_1.pt"
+torch.save({"X": torch.tensor(X_norm), "Y": torch.tensor(Y_norm)}, save_path)
+print("Saved normalized train_1_time_step dataset:", save_path)
+
